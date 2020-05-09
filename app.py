b871c80e87f0c1f6536487957d410b26b30b5784
Michael Gale <michael@michaelgale.dev>
2020-05-09 12:28:05 +1000

    split in func

--- app.py
+++ app.py
@@ -2,29 +2,36 @@
 def read_data(filename):
   data_file = open(filename,'r')
   lines = data_file.readlines()
-  output = []
+  output = {}
   for line in lines:
-    line=line.replace('\n', '')
-    output.append(line)
+    line = line.replace('\n', '')
+    line = line.split(',')
+    output_val = line[1]
+    output[line[0]] = output_val
+
   data_file.close()
   return output
-
 
 def get_average_dictionary(readings):
   return {}
 
 FILENAME = '/home/michael/studies/python_assignment2/days.txt'
 
-if __name__ == "__main__":
-  try:
-    readings = read_data(FILENAME)
-    averages = get_average_dictionary(readings)
+print(read_data(FILENAME))
 
-    # Loops through the keys in averages, sorted from that with the largest associated value in averages to the lowest - see https://docs.python.org/3.5/library/functions.html#sorted for details
-    for location in sorted(averages, key = averages.get, reverse = True):
-      print(location, averages[location])
 
-  except (IOError, ValueError):
-    print("Error reading {}".format(FILENAME))
-    print("Please ensure the file exists and matches the required format")
-    print("(each line should begin with a location name, followed by a comma, followed by an AQI reading)")
+
+
+# if __name__ == "__main__":
+#   try:
+#     readings = read_data(FILENAME)
+#     averages = get_average_dictionary(readings)
+
+#     # Loops through the keys in averages, sorted from that with the largest associated value in averages to the lowest - see https://docs.python.org/3.5/library/functions.html#sorted for details
+#     for location in sorted(averages, key = averages.get, reverse = True):
+#       print(location, averages[location])
+
+#   except (IOError, ValueError):
+#     print("Error reading {}".format(FILENAME))
+#     print("Please ensure the file exists and matches the required format")
+#     print("(each line should begin w`ith a location name, followed by a comma, followed by an AQI reading)")
