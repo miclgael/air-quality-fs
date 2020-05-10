7468cfe196ddd09667c7dd857da3ea337e554e02
Michael Gale <michael@michaelgale.dev>
2020-05-10 12:47:05 +1000

    it will warn you if formatting wrong (comma)

--- app.py
+++ app.py
@@ -1,20 +1,44 @@
 
 def read_data(filename):
+
+  def colorize(text, msg_type="error"):
+    if msg_type == "error" or msg_type == None:
+      start = "\u001b[31mError:" # <- ANSI for "red"
+    elif msg_type == 'warning' or msg_type == "warn": 
+      start = "\u001b[33mWarning:" # <- ANSI for "red"
+    elif msg_type == "success": 
+      start = "\u001b[32mSuccess!" # <- ANSI for "red"
+    end = "\033[0m"  # <- ANSI for "normal"
+    return "{} {} {}".format(start,text,end)
+
   data_file = open(filename,'r')
   lines = data_file.readlines()
   output = {}
-  for line in lines:
-    line = line.replace('\n', '')
-    line = line.split(',')
-    location = line[0]
-    value = line[1]
+  for (index,line) in enumerate(lines):
+    ## Check for formatting error (comma-space)
+    if (', ' in line):
+      print(colorize(
+        'Please check formatting! Linter (comma-space) expected "," but found ", " in file "./{1}", line {0}'.format(index+1, filename), 
+        'warn'
+      ))
+      line = line.replace(", ", ",") # auto-fix space.
+   
+    line = line.replace('\n', '') # remove extraneous new lines
+    line = line.split(',')        # split at all commas
+    location = line[0]            # 
+    value = line[1]               #
+    
+    try:
+      output[location] = float(value)
+    except ValueError:
+      print('dipshit!')
 
     # Eg, does 'Saturday' already exist?
-    if output[location] in output:
-      output[location] = [output[location]]
-      output[location].append(value)
-    else:
-      output[location] = value
+    # if output[location] in output:
+    #   output[location] = [output[location]]
+    #   output[location].append(value)
+    # else:
+    #   output[location] = value
 
   data_file.close()
   return output
